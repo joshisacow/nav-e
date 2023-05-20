2ad3ce52d6a2f4c02f492f14eb57f4422f05723e
Joshua Chen <joshua.chen@duke.edu>
2023-05-19 23:20:58 -0700

    modified flask http protocol

--- index.py
+++ index.py
@@ -6,13 +6,36 @@ trips = [
     { 'tripID': ['routes'] }
 ]
 
+@app.route('/trip/<tripID>', methods=['GET', 'PUT', 'DELETE'])
+def tripId(tripID):
+    if request.method == 'GET':
+        return jsonify(trips[int(tripID)])
+    elif request.method == 'PUT':
+        trips[int(tripID)] = request.get_json()
+        return '', 204
+    elif request.method == 'DELETE':
+        trips.pop(int(tripID))
+        return '', 204
+    else:
+        return '', 405
 
-@app.route('/trip')
-def get_trips():
-    return jsonify(trips)
 
 
-@app.route('/trip', methods=['POST'])
-def add_trip():
-    trips.append(request.get_json())
-    return '', 204
+@app.route('/trips', methods=['POST', 'GET', 'PUT', 'DELETE'])
+def trips():
+    if request.method == 'POST':
+        trips.append(request.get_json())
+        return '', 204
+    elif request.method == 'GET':
+        return jsonify(trips)
+    # elif request.method == 'PUT': not sure if this is needed
+
+    elif request.method == 'DELETE':
+        trips.clear()
+        return '', 204
+    else:
+        return '', 405
+    
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
