c18a7135cd661b93b78e09b05f092dc38546a01a
Joshua Chen <joshua.chen@duke.edu>
2023-06-12 18:04:42 -0700

    Search bar created (#17)
    * deleted nav-e folder inside frontend
    
    * rendered basic map
    
    * changed gmaps api key
    
    * created map and location pin components
    
    * minor changes to css
    
    * added working search bar
    
    * removed node modules
    
    * removed imports

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,49 +1,22 @@
 "use client"
 
 import Image from 'next/image'
-import { useMemo } from 'react'
-import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api'
+import { useState } from 'react'
+import { useLoadScript } from '@react-google-maps/api'
+import Map from '@/components/Map'
+import SearchBar from '@/components/SearchBar'
 
 
 export default function Home() {
   const { isLoaded, loadError } = useLoadScript({ 
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    libraries: ['places']
   });
 
   if (!isLoaded) return 'Loading...';
   if (loadError) return 'Error loading maps';
 
-  return <Map />;
-
-
-    // <main className="flex min-h-screen flex-col justify-between p-24">
-    //   Nav-E
-      
-
-    // </main>
-}
-
-function Map() {
-  const mapContainerStyle = {
-    width: '100%',
-    height: '100vh',
-  };
-
-  const center = useMemo( () => ({
-    lat: 44,
-    lng: -80,
-  }), []);
-
-  return (
-    <GoogleMap 
-      zoom ={10} 
-      center={center} 
-      mapContainerStyle = {mapContainerStyle}> 
-      <Marker 
-        position={center} 
-        // icon = {"http://maps.google.com/mapfiles/ms/icons/blue-dot.png"}
-      />
-    </GoogleMap>
-
+  return ( <Map />
   )
 }
+
---
+++ frontend/src/components/Map.jsx
@@ -0,0 +1,52 @@
+import React, {useMemo, useCallback} from 'react'
+import {GoogleMap} from '@react-google-maps/api'
+import LocationPin from '@/components/LocationPin'
+import SearchBar from '@/components/SearchBar'
+
+const Map = (props) => {
+    const [pan, setPan] = React.useState();
+    const mapRef = React.useRef();
+    const center = useMemo( () => ({
+        lat: 33.68,
+        lng: -117.83,
+        // TODO: change to user's location
+    }), []);
+
+    const options = useMemo( () => ({
+        streetViewControl: false,
+        mapTypeControl: false,
+        fullscreenControl: false,
+        clickableIcons: false,
+        mapId: "b6a8170c1c0be23f",
+    }), []);
+    const onLoad = useCallback((map) => (mapRef.current = map), []);
+
+    return (
+        <div class="wrapper">
+            <div className="search-bar">
+                <h1>Nav-E</h1> 
+                <SearchBar setPan = {(position) => {
+                    setPan(position);
+                    mapRef.current?.panTo(position);
+                }} />
+            </div>
+            <GoogleMap 
+                zoom ={10} 
+                center={center} 
+                mapContainerClassName = "map-container"
+                options = {options}
+                onLoad = {onLoad}
+
+            >   
+                {pan && <LocationPin  position = {pan} />}
+                <LocationPin 
+                    position = {center}
+                    icon = {"http://maps.google.com/mapfiles/ms/icons/red-dot.png"}
+                />
+            </GoogleMap>
+        </div>
+    )
+}
+
+
+export default Map;
