46bb73e669cf898387afb63997599a0ebd2d56f3
Joshua Chen <joshua.chen@duke.edu>
2023-06-12 17:53:19 -0700

    removed imports

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,8 +1,6 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
-import React from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
+import React from 'react'
 
 export const metadata = {
   title: 'Nav-E',
