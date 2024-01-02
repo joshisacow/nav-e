de95ebbc15284d6fda76eec702456b3aef8b7753
Josh Chen <joshchen2003@gmail.com>
2024-01-02 06:00:16 -0500

    added guards and rating display

--- frontend/src/components/map/CustomWindow.jsx
+++ frontend/src/components/map/CustomWindow.jsx
@@ -1,19 +1,52 @@
 import '@/styles/CustomWindow.css';
+import Rating from '@mui/material/Rating';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDollarSign } from '@fortawesome/free-solid-svg-icons';
 
 const CustomWindow = ({ info, addCurrentToTrip }) => {
+
+    if (info.price_level) {
+        const price = Array(info.price_level).fill(<FontAwesomeIcon icon={faDollarSign} />);
+        console.log(price);
+    }
+
     return (
         <div className = "info-window-container">
-            <h1>{info.name}</h1>
-            <h1>{info.formatted_phone_number}</h1>
+            <div className="info-header">
+                <h1 className = "text-xl">{info.name}</h1>
+                {info.rating && info.user_ratings_total && 
+                    (
+                        <>
+                            {info.rating}
+                            <Rating 
+                                name="user-rating"
+                                value={info.rating}
+                                readOnly
+                                precision={0.1}
+                                />
+                            ({info.user_ratings_total}) 
+
+                            {info.price_level && 
+                                [...Array(info.price_level)].map((e, i) =>(
+                                    <FontAwesomeIcon id={i+1} className="text-gray-500" icon={faDollarSign} />
+                                ))
+                            }
+                        </>
+                    )
+                }
+            </div>
             <h1>{info.formatted_address}</h1> 
-            <h1>{info.business_status}</h1>
-            <h2>{info.editorial_summary.overview}</h2>
-            <h3>{info.price_level}</h3>
-            <h3>{info.rating}</h3>
-            <h3>{info.user_ratings_total}</h3>
-            <h3>{info.website}</h3>
-            <h3>{info.url}</h3>
-            <h4>{info.opening_hours.weekday_text}</h4>
+            {info.formatted_phone_number && <h1>{info.formatted_phone_number}</h1>}
+            {info.business_status && <h1>{info.business_status}</h1>}
+            {info.editorial_summary && <h1>{info.editorial_summary.overview}</h1>}
+            {info.website && <h1>{info.website}</h1>}
+            {info.url && <h1>{info.url}</h1>}
+            {info.opening_hours && 
+                <>
+                    <h1>{info.opening_hours.open_now ? "Open" : "Closed"}</h1>
+                    <h1>{info.opening_hours.weekday_text}</h1>
+                </>
+            }
 
             <button onClick = {() => addCurrentToTrip()} className="add-button">
                 Add to Trip
