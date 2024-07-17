a157d004ca7989b32ff6d829264e3b15f51d2997
Josh Chen <j.chen@draftkings.com>
2024-07-17 10:49:49 -0400

    fixed build errors + moved text to locale file

--- frontend/src/components/map/HelpModal.jsx
+++ frontend/src/components/map/HelpModal.jsx
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import IconButton from '../utils/IconButton';
 import "@/styles/HelpModal.css";
+import { EN_US } from '../../locales/EN-US';
 
 const style = {
     position: 'absolute',
@@ -26,10 +27,10 @@ const HelpModal = ({ openHelpModal, handleClose }) => {
         >
             <Box sx={style}>
                 <Typography id="modal-modal-title" variant="h6" component="h2">
-                    Get Started with Nav-E
+                    {EN_US.HELP_MODAL.TITLE}
                 </Typography>
                 <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                    Nav-E is a tool built to make visualizing and planning multi-stop iterinaries more intuitive for users. The main benefit is the ability to visualize all potential destinations before actually choosing the destinations you want to add to your intinerary based on the information laid out. To get started, just add all locations you were considering visitng to the 'considering' category. Or, if you're sure about specific locations, add it to 'decided' which will directly add it to your itinerary. Based on the layout of the locations, you have the power to choose which destinations should be included in your itinerary intuitively, and you can click on those destinations and add them to your itinerary ('decided' button). After choosing all the locations you want, you can reorder them by dragging the locations into your desired order or use the optimize route button to find the shortest path through all the points. Then, you can build the path and click the trip text to open the trip in Google Maps or save the route to your profile.   
+                    {EN_US.HELP_MODAL.DESCRIPTION}
                 </Typography>
                 <IconButton
                     icon="close"
---
+++ frontend/src/locales/EN-US.ts
@@ -0,0 +1,6 @@
+export const EN_US = {
+    HELP_MODAL: {
+        TITLE: "Get Start with Nav-E",
+        DESCRIPTION: 'Nav-E is a tool built to make visualizing and planning multi-stop iterinaries more intuitive for users. The main benefit is the ability to visualize all potential destinations before actually choosing the destinations you want to add to your intinerary based on the information laid out. To get started, just add all locations you were considering visitng to the \'considering\' category. Or, if you\'re sure about specific locations, add it to \'decided\' which will directly add it to your itinerary. Based on the layout of the locations, you have the power to choose which destinations should be included in your itinerary intuitively, and you can click on those destinations and add them to your itinerary (\'decided\' button). After choosing all the locations you want, you can reorder them by dragging the locations into your desired order or use the optimize route button to find the shortest path through all the points. Then, you can build the path and click the trip text to open the trip in Google Maps or save the route to your profile.'
+    }
+}
